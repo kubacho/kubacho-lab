0d560ce24516187fb9a465566394082661dd3535
Kubacho <kubacho.md@gmail.com>
2023-03-10 19:34:05 +0700

    pre webgl

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,15 @@
 
 
 
-var canvas = document.getElementById('canvas');
+// var canvas = document.getElementById('glslCanvas');
+// console.log(canvas);/
+var canvas = document.createElement("canvas");
+var glslCanvas = new glslCanvas(canvas);
+
+// fetch('shader.frag')
+//     .then(response => response.text())
+//     .then((data) => {
+//         console.log(data);
+//         glslCanvas.load(data);
+//     })
+
